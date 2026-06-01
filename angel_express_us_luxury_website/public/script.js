e3cf756f2c625dfc91c8968d7de5d80ae9c2b3b7
angelsexpresss-rgb <angelsexpresss@gmail.com>
2026-06-01 00:31:02 -0500

    Update script.js

--- angel_express_us_luxury_website/public/script.js
+++ angel_express_us_luxury_website/public/script.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = "https://zlzastjpvbboniybyyjv.supabase.co";
+console.log("SUPABASE_URL =", SUPABASE_URL);
 
 const SUPABASE_ANON_KEY = "sb_publishable_Fn0wUeIUskON-kTpl8kDFw_B1Exp0EP";
 
