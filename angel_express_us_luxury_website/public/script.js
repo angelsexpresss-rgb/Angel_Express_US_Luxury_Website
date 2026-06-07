2008aeb68525caab0b88f91b2cd5fb54d521527a
angelsexpresss-rgb <angelsexpresss@gmail.com>
2026-06-07 05:13:32 -0500

    Update script.js

--- angel_express_us_luxury_website/public/script.js
+++ angel_express_us_luxury_website/public/script.js
@@ -104,7 +104,7 @@ async function generateInvoicePDF(booking) {
   doc.text("Angel Express", 50, 140);
   doc.text("Phone: 617-606-0679 | Email: angelsexpresss@gmail.com", 50, 155);
   doc.text("Reservation Link: https://angelexpressus.com/", 50, 170);
-  
+
 doc.setFontSize(11);
 doc.setFont(undefined, "bold");
 
